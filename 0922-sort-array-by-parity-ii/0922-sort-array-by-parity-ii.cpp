912d786b0191b40742969549f2911ba93e39135f
sar.thak <sarthakbrid9c24@gmail.com>
2026-07-03 20:42:38 +0530

    Time: 0 ms (100%), Space: 26 MB (51.59%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
@@ -1,27 +1,19 @@
-//brute force
-//put number in even places if its even otherwise at odd places
+//move i till it hits odd and move j till it hits even
+//if they hit someone like that swap them no matter what
 
 class Solution {
 public:
     vector<int> sortArrayByParityII(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> result(n, 0);       //to make vector of n zeroes first declare size then value (0) in brackets
-        int i = 0, j = 0, k = 1;
+        int i = 0, j = 1, n = nums.size();
 
-        while(i < n){
-            if(nums[i] % 2 == 0){
-                result[j] = nums[i];
-                j = j + 2;
-            }
+        while (i < n && j < n) {
+            while (i < n && nums[i] % 2 == 0)   i += 2;                
 
-            else{
-                result[k] = nums[i];
-                k = k + 2;
-            }
+            while (j < n && nums[j] % 2 == 1)   j += 2;                
 
-            i++;
+            if (i < n && j < n)     swap(nums[i], nums[j]);                
         }
 
-        return result;
+        return nums;
     }
 };
