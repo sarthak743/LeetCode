0ada78c6e8125f0ae9b36ff3935dca082ea83d25
sar.thak <sarthakbrid9c24@gmail.com>
2026-07-08 21:42:46 +0530

    Time: 0 ms (100%), Space: 83.8 MB (7.17%) - LeetHub

---
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
@@ -0,0 +1,13 @@
+//check the explaination
+//we get those number by xoring the number at that position and number prev to it from pref array
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        vector<int> result;
+        result.push_back(pref[0]);
+        for(int i = 1; i < pref.size(); i++){
+            result.push_back(pref[i-1] ^ pref[i]);
+        }
+        return result;
+    }
+};
