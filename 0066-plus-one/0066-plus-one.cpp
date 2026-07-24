565396b015736552486161e64e194aa21ac9e9fc
sar.thak <sarthakbrid9c24@gmail.com>
2026-07-24 23:57:04 +0530

    Time: 0 ms (100%), Space: 11.7 MB (28.14%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        digits.insert(digits.begin(), 1);       //this will execute if first number gets to be 9
+        return digits;
+    }
+};
