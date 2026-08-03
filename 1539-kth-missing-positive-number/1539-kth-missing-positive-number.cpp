0f8a48adf258690c65e032110612d5a639051597
sar.thak <sarthakbrid9c24@gmail.com>
2026-08-03 21:47:04 +0530

    Time: 0 ms (100%), Space: 13.3 MB (61.11%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,14 +1,24 @@
-//brute force
-//TC - O(N)
+//binary search
+//refer notes or video for explaination
+//TC - O(logN)
 
 class Solution {
 public:
+    int no_of_missing_numbers(int a, vector<int>& arr){
+        return arr[a] - a - 1;
+    }
+
     int findKthPositive(vector<int>& arr, int k) {
-        for(int i = 0; i < arr.size(); i++){
-            if(arr[i] <= k) k++;
-            else break;
+        int low = 0, high = arr.size() - 1;
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+
+            if(no_of_missing_numbers(mid, arr) >= k)  high = mid - 1;
+            else low = mid + 1;
         }
 
-        return k;
+        if(high == -1) return k;        //edge case
+        return arr[high] + k - no_of_missing_numbers(high, arr);
     }
 };
