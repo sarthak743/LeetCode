3b757b098a1c1efee246d3fde1c62b1ce7f0a9b6
sar.thak <sarthakbrid9c24@gmail.com>
2026-08-05 19:59:28 +0530

    Time: 0 ms (100%), Space: 95.1 MB (62.01%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,14 +1,16 @@
 //too complex to understand 
 //skip it for most of the time
 //optimal solution is BINARY SEARCH
+//this is cleaner version
 
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 
-        // Always binary search on the smaller array
-        if(nums1.size() > nums2.size())
-            return findMedianSortedArrays(nums2, nums1);        //this return makes function to take the smaller array
+        // Always perform binary search on the smaller array.
+        // This keeps the time complexity O(log(min(n1, n2))).
+        if (nums1.size() > nums2.size())
+            return findMedianSortedArrays(nums2, nums1);
 
         int n1 = nums1.size();
         int n2 = nums2.size();
@@ -16,26 +18,47 @@ public:
         int low = 0;
         int high = n1;
 
-        while(low <= high){
+        while (low <= high) {
 
-            int cut1 = (low + high)/2;
-            int cut2 = (n1 + n2 + 1)/2 - cut1;
+            // Partition (cut) position in first array.
+            int cut1 = low + (high - low) / 2;
 
-            int left1 = (cut1==0) ? INT_MIN : nums1[cut1-1];
-            int left2 = (cut2==0) ? INT_MIN : nums2[cut2-1];
-            int right1 = (cut1==n1) ? INT_MAX : nums1[cut1];
-            int right2 = (cut2==n2) ? INT_MAX : nums2[cut2];
+            // Partition position in second array.
+            // Total elements on the left side should be (n1+n2+1)/2.
+            int cut2 = (n1 + n2 + 1) / 2 - cut1;
 
-            if(left1<=right2 && left2<=right1){
-                if((n1+n2)%2==0)    return (max(left1, left2) + min(right1,right2)) / 2.0;
+            // Elements immediately left of both partitions.
+            int left1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
+            int left2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
 
+            // Elements immediately right of both partitions.
+            int right1 = (cut1 == n1) ? INT_MAX : nums1[cut1];
+            int right2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
+
+            // Correct partition found.
+            if (left1 <= right2 && left2 <= right1) {
+
+                // Even total number of elements.
+                if ((n1 + n2) % 2 == 0) {
+                    return (max(left1, left2) + min(right1, right2)) / 2.0;
+                }
+
+                // Odd total number of elements.
                 return max(left1, left2);
             }
 
-            else if(left1>right2)   high=cut1-1;
-            else    low=cut1+1;
+            // We have taken too many elements from nums1.
+            if (left1 > right2) {
+                high = cut1 - 1;
+            }
+
+            // We have taken too few elements from nums1.
+            else {
+                low = cut1 + 1;
+            }
         }
 
-        return 0;
+        // This line is never reached.
+        return 0.0;
     }
 };
