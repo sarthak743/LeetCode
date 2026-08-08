c49e0b2b1d12d871575c568751722d4ef864c2f1
sar.thak <sarthakbrid9c24@gmail.com>
2026-08-08 11:53:53 +0530

    Time: 0 ms (100%), Space: 9.8 MB (49.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,36 +1,19 @@
-//first we will compare the size of the strings if yes then move forward no then false
-//we will use hashing twice bcecause of two different strings
-//further we will compare these two if same then its anagram
-//while comparing the loop should go from a to z to check the number of alphabets present in those maps
-//while hashing we used <char, int> so the number of characters is stored in the map 
-//Ex : {{a, 3}, {n, 1}, {g, 1}, {r, 1}, {m, 1}} --> for word "anagram" also for "nagaram" since the maps are same they are anagram
+//cleaner hash method
 
 class Solution {
 public:
-    bool isAnagram(string s, string t) 
-    {
-        if(s.size()!=t.size())
-        {
-            return false;
-        }
+    bool isAnagram(string s, string t) {
+        vector<int> mpp1(256, 0);
+        vector<int> mpp2(256, 0);
 
-        int n = s.size();
-        unordered_map<char, int> mpp1;
-        unordered_map<char, int> mpp2;
+        if(s.size() != t.size())    return false;
 
-        for(int i=0; i<n; i++)
-        {
-            mpp1[s[i]]++;     
-            mpp2[t[i]]++;  
-        }
+        for(int i = 0; i < s.size(); i++){
+            mpp1[s[i]]++;
+            mpp2[t[i]]++;
+        }       
 
-        for(char j='a'; j<='z'; j++) 
-        {
-            if(mpp1[j]!=mpp2[j])
-            {
-                return false;
-            }
-        }
-        return true;
+        //return true if both hash table are equal
+        return mpp1 == mpp2;
     }
 };
