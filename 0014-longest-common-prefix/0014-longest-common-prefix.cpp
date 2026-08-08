2dc75be5f02fb45d04a06c9f37a31d93d25c54e7
sar.thak <sarthakbrid9c24@gmail.com>
2026-08-08 11:29:03 +0530

    Time: 0 ms (100%), Space: 11.9 MB (38.65%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,50 +1,29 @@
-//erase();
-//s = "abcd";
-//s.erase(2) --> output : s = "ab"
-//everything from s[2] till end gone
-
+//cleaner version
 
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string s = "";
-        if(strs.size() == 0) return s;      
-        if(strs.size() == 1) return strs[0];
+        string prefix = strs[0];
 
-        //if empty string exists then its the answer 
-        for(int i = 0; i < strs.size(); i++){
-            if(strs[i] == "") return s;
-        }
-
-        int i = 0;
-        while (i < min ( strs[0].size(), strs[1].size() ) ){
-            if(strs[0][i] != strs[1][i])    break;
-            s += strs[0][i];
-            i++;
-        }
-
-        for(int i = 2; i < strs.size(); i++){
+        for (int i = 1; i < strs.size(); i++) {
             int j = 0;
-            while(j < min (s.size(), strs[i].size() ) ){
-                //checks if character is same or not, if not then everything from that character till end of s will get erased-
-                if(s[j] != strs[i][j]){
-                    s.erase(j);     //this will remove (erase) characters from s[j] till the end
-                    break;
-                }
-
+        
+            while (j < prefix.size() &&
+                   j < strs[i].size() &&
+                   prefix[j] == strs[i][j]) {
                 j++;
             }
 
-            //for that fucking edge case
-            if(s.empty())   return "";
+            //now prefix will have substring of prefix
+            //starting from index 0
+            //will have in total j characters
+            prefix = prefix.substr(0, j);
 
-            //when strs[i] is shorter than s this if will help
-            if(j == strs[i].size() && strs[i].size() < s.size()){
-                s.erase(strs[i].size());
-            }
-            //this should outside of while loop coz this condition cant be checked inside of while loop
+            //this cancel out edge case
+            if (prefix.empty())
+                return "";
         }
 
-        return s;
+        return prefix;
     }
 };
