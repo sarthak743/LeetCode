fc32b8b2e086ad582a0763138e4ef997cc874e87
sar.thak <sarthakbrid9c24@gmail.com>
2026-08-16 11:27:51 +0530

    Time: 0 ms (100%), Space: 178.6 MB (64.52%) - LeetHub

--- 4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights/4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights.cpp
+++ 4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights/4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights.cpp
@@ -1,36 +1,19 @@
-//get remainder of every car arrival time
-//search if theres a light greater than remainder
-//if not found append the (period - rem) else 0
-//to optimize search use binary search
+//more optimal 
+//penalty only gets updated when we dont find light greater than remainder
 
 class Solution {
 public:
-    bool isfound(int rem, vector<int>& arr){
-        int low = 0, high = arr.size() - 1;
-
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            if(arr[mid] > rem)
-                return true;
-            else 
-                low = mid + 1;
-        }
-
-        return false;
-    }
-
-    
     int minPenalty(int period, vector<int>& lights, vector<int>& arrivalTime) {
-        int rem = 0;
-        int waitin = 0;
-        sort(lights.begin(), lights.end());
-        
+        int maxi = *max_element(lights.begin(), lights.end());
+        int result = 0;
+
         for(int i = 0; i < arrivalTime.size(); i++){
-            rem = arrivalTime[i] % period;
-            if(!isfound(rem, lights))
-                waitin = max(waitin, period - rem);            
+            int r = arrivalTime[i] % period;
+
+            if(r >= maxi)
+                result = max(result, period - r);
         }
 
-        return waitin;
+        return result;
     }
 };
