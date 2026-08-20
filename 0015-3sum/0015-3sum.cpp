5295791cbf8241832b89f271db3297dd049ce805
sar.thak <sarthakbrid9c24@gmail.com>
2026-08-20 18:29:37 +0530

    Time: 59 ms (19.46%), Space: 31.1 MB (20.57%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,41 @@
+//no duplicates meaning if array is [1, -1, 0, 0] then triplets [1, -1, 0] with arr[2] and [1, -1, 0] with arr[3] are generated but they are excatly same so only one will be counted
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) 
+    {
+        int n = nums.size();
+        vector<vector<int>> result;
+
+        sort(nums.begin(), nums.end());
+
+        for(int i=0; i<n; i++)
+        {
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            int j = i + 1;
+            int k = n - 1;
+
+            while(j < k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if(sum < 0)     j++;
+
+                else if(sum > 0)    k--;
+
+                else
+                {
+                    vector<int> temp = {nums[i], nums[j], nums[k]};
+                    result.push_back(temp);
+                    j++;
+                    k--;
+
+                    while(j < k && nums[j] == nums[j-1])    j++;
+                    while(j < k && nums[k] == nums[k+1])    k--;
+                }
+            }
+        }
+
+        return result;
+    }
+};
