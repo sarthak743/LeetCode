ab0a9712480558dd7ff0516d90f3708fc8f07d41
sar.thak <sarthakbrid9c24@gmail.com>
2026-08-25 00:10:54 +0530

    Time: 0 ms (100%), Space: 19.8 MB (8.58%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -8,6 +8,12 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//make new dummy list
+//append lower value in it 
+//make new node put that value 
+//make temp point to it 
+//then increment temp
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
@@ -21,7 +27,7 @@ public:
         //append lower value in dummy
         while(temp1 && temp2){
             if(temp1->val <= temp2->val){
-                //new node 
+                //new node will get added next to temp
                 temp->next = new ListNode(temp1->val, nullptr);
                 temp1 = temp1->next;
             }
