659450b20b2904cd7c294ab8e1d9289999817658
sar.thak <sarthakbrid9c24@gmail.com>
2026-08-25 22:46:45 +0530

    Time: 0 ms (100%), Space: 13.7 MB (8.3%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,5 +1,3 @@
-const size_t BUFFER_SIZE = 0x6fafffff; alignas(std::max_align_t) char buffer[BUFFER_SIZE]; size_t buffer_pos = 0; void* operator new(size_t size) { constexpr std::size_t alignment = alignof(std::max_align_t); size_t padding = (alignment - (buffer_pos % alignment)) % alignment; size_t total_size = size + padding; char* aligned_ptr = &buffer[buffer_pos + padding]; buffer_pos += total_size; return aligned_ptr; } void operator delete(void* ptr, unsigned long) {} void operator delete(void* ptr) {} void operator delete[](void* ptr) {}
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -12,24 +10,22 @@ const size_t BUFFER_SIZE = 0x6fafffff; alignas(std::max_align_t) char buffer[BUF
  */
 
 //OPTIMAL
-//ITERATIVE
+//RECURSIVE
 
-//3 pointers
+//check video for better understanding
 
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* temp = head;      //curr
-        ListNode* prev = nullptr;   //prev
-        ListNode* front = nullptr;  //front
+        //base case
+        if(!head || !head->next)
+            return head;
 
-        while(temp){
-            front = temp->next;     //keep front ahead of curr
-            temp->next = prev;      //make curr point to prev
-            prev = temp;            //move prev ahead
-            temp = front;           //move temp ahead too
-        }
+        ListNode* newhead = reverseList(head->next);
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = nullptr;
 
-        return prev;        //new head will be at end which is prev
+        return newhead;
     }
 };
