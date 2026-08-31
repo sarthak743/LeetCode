c1e1fa944d5cd2e192fa9cc01b83814f2cd5d9b0
sar.thak <sarthakbrid9c24@gmail.com>
2026-09-01 00:40:44 +0530

    Time: 0 ms (100%), Space: 14.9 MB (45.64%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -8,57 +8,39 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//make dummy nodes at beginning
+//for both lower and higher val
+//update the links acc to val of head
+//at end update the final link
+//this handles edge cases very well
+
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        if(!head || !head->next)
-            return head;
+        ListNode* lowdummy = new ListNode(INT_MIN, nullptr);
+        ListNode* highdummy = new ListNode(INT_MIN, nullptr);
 
-        ListNode* high = nullptr;       //track higher and equal to val
-        ListNode* low = nullptr;        //track lower val
-        ListNode* fhigh = nullptr;      //track first higher and equal
-        ListNode* flow = nullptr;       //track first lower
-        ListNode* temp = head;
+        ListNode* low = lowdummy;
+        ListNode* high = highdummy;
 
-        while(temp){
-            if(temp->val < x){
-                if(!low){
-                    low = temp;
-                    flow = low;
-                }
-                    
-                else{
-                    low->next = temp;
-                    low = temp;
-                }
+        while(head){
+            if(head->val < x){
+                low->next = head;
+                low = head;
             }
 
             else{
-                if(!high){
-                    high = temp;
-                    fhigh = high;
-                }
-
-                else{
-                    high->next = temp;
-                    high = temp;
-                }
+                high->next = head;
+                high = head;
             }
 
-            temp = temp->next;
+            head = head->next;
         }
 
-        //when all nodes are higher
-        if(!flow)
-            return fhigh;
-
-        //when all nodes are lower
-        if(!fhigh)
-            return flow;
-
-        low->next = fhigh;
+        low->next = highdummy->next;
         high->next = nullptr;
 
-        return flow;
+        return lowdummy->next;
     }
 };
