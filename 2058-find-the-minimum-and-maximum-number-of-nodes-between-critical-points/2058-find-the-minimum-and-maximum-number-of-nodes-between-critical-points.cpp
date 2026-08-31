e6257ff1039bb321e9af3d8dce96f51d412b8287
sar.thak <sarthakbrid9c24@gmail.com>
2026-08-31 15:20:35 +0530

    Time: 7 ms (33.43%), Space: 124.6 MB (60.57%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -8,68 +8,47 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//done with no extra space
+
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        //for two and three nodes
-        if(!head->next->next || !head->next->next->next)
-            return {-1, -1};
-        
-        //making separate array and working on it
-        vector<int> arr;
-        ListNode* temp = head;
-        while(temp){
-            arr.push_back(temp->val);
-            temp = temp->next;
-        }
+        ListNode* prev = head;
+        ListNode* curr = head->next;
 
-        //array for critical nodes
-        vector<int> cric(arr.size(), 0);
-        for(int i = 1; i < arr.size() - 1; i++){
-            if(arr[i] > arr[i+1] && arr[i] > arr[i-1])
-                cric[i]++;
-            if(arr[i] < arr[i+1] && arr[i] < arr[i-1])
-                cric[i]++;
-        }
+        int mini = INT_MAX, maxi = -1;
 
-        int mini = -1, maxi = -1;
+        int pos = 1;
+        int first = -1, last = -1;
+        while(curr && curr->next){
+            bool cric = false;
+            cric = ((curr->val > prev->val && curr->val > curr->next->val) || 
+                    (curr->val < prev->val && curr->val < curr->next->val));
 
-        //finding first and last critical
-        int l = 0;                  //stores pos of first critical
-        int r = cric.size() - 1;    //stores pos of last critical   
-        while(l < cric.size()){
-            if(cric[l] == 1)
-                break;
-            l++;
-        }
-
-        while(r >= 0){
-            if(cric[r] == 1)
-                break;
-            r--;
-        }
-
-        //update only if we find
-        //distinct criticals
-        if(l < r){
-            mini = cric.size();
-            maxi = r - l;
-
-            //put r ahead of l
-            //if r hit critical get distance 
-            //update that distance in mini
-            //put l in r
-            r = l + 1;
-            while(r < cric.size()){
-                if(cric[r] == 1){
-                    mini = min(mini, r - l);
-                    l = r;
-                }
-                r++;
+            if(cric && first == -1){
+                first = pos;
+                last = pos;
             }
-        }
+                                
             
+            if(cric && pos != first){                
+                mini = min(mini, pos - last);
+                last = pos;
+            }
 
-        return {mini, maxi};        
+            pos++;
+            prev = curr;
+            curr = curr->next;
+        }
+
+        maxi = last - first;
+
+        if(first == last){
+            mini = -1;
+            maxi = -1;
+        }           
+
+        return {mini, maxi};
     }
 };
