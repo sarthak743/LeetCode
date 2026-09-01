fe7b381a4519d38794368774f5738554b61c482a
sar.thak <sarthakbrid9c24@gmail.com>
2026-09-01 23:27:49 +0530

    Time: 53 ms (76.87%), Space: 24 MB (55.75%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -7,55 +7,43 @@
  * };
  */
 
-//get head of longer list ahead 
-//so that both head have to 
-//travel same distance till nullptr
-//after that move both simultaneously by one 
-//if they hit then true else false
+//iterate both
+//when t1 reach nullptr put t1 to start of other list
+//same for other
+//theyll hit if intersection exists
+//also both travel same distance at the end (sum of len of both list)
 
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int a = 0, b = 0, d = 0;
+        if(!headA || !headB)    
+            return nullptr;
+
         ListNode* t1 = headA;
         ListNode* t2 = headB;
 
-        while(t1){
-            a++;
+        //loop terminates when both r equal
+        //1. when theres intersection
+        //2. when both reach nullptr (after tail) of respective lists
+        while(t1 != t2){     
             t1 = t1->next;
-        }
-
-        while(t2){
-            b++;
             t2 = t2->next;
+
+            //immediately check 
+            //else it will go infinite loop
+            if(t1 == t2)
+                return t1;       
+
+            //for first time t1 reaches null
+            //send it back to headB
+            if(!t1)
+                t1 = headB;
+            if(!t2)
+                t2 = headA;            
         }
 
-        //if both r of same length
-        //while loop below doesnt run
-        //keeping both heads at same location
-        d = abs(a-b);
-        
-        //if A is longer
-        //move headA
-        if(a > b){
-            while(d--)
-                headA = headA->next;
-        }
-
-        //if B is longer
-        //move headB
-        else{
-            while(d--)
-                headB = headB->next;
-        }
-        
-        while(headA && headB){
-            if(headA == headB)
-                return headA;
-            headA = headA->next;
-            headB = headB->next;
-        }
-
-        return nullptr;
+        //returns intersection node if exists
+        //if no t1 reaches nullptr which is good to return it as it is
+        return t1;
     }
 };
