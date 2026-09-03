563d0cff441920df9d6b1877fe31ee4237135f03
sar.thak <sarthakbrid9c24@gmail.com>
2026-09-03 23:43:45 +0530

    Time: 0 ms (100%), Space: 16.4 MB (70.45%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -58,8 +58,8 @@ public:
             //for last group 
             //if it has less than k nodes
             if(!kThNode){
-                if(prevLast)
-                    prevLast->next = temp;
+                // if(prevLast)
+                prevLast->next = temp;
                 break;
             }
 
