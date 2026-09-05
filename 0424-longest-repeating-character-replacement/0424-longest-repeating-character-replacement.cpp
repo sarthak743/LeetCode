b45de8f13025e4cba4a44238a202608e2f10bd6a
sar.thak <sarthakbrid9c24@gmail.com>
2026-09-05 18:58:59 +0530

    Time: 7 ms (44.15%), Space: 10.8 MB (71.48%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -18,7 +18,7 @@ public:
             hash[s[r] - 'A']++;
             maxf = max(maxf, hash[s[r] - 'A']);
 
-            while((r - l + 1) - maxf > k){
+            if((r - l + 1) - maxf > k){
                 hash[s[l] - 'A']--;
                 l++;
 
