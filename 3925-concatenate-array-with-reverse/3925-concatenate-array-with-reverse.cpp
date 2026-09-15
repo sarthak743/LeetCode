206a0c34fb4a1767aab37233d85d11644e65c777
sar.thak <sarthakbrid9c24@gmail.com>
2026-09-15 20:12:41 +0530

    Time: 3 ms (23.04%), Space: 30.6 MB (49.37%) - LeetHub

---
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    vector<int> concatWithReverse(vector<int>& nums) {
+        for(int i=nums.size() - 1; i >= 0; i--)
+            nums.push_back(nums[i]);
+        return nums;
+    }
+};
